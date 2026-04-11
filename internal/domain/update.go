012de9f3cfdebb7279d5e26911a4cfb161524ff3
alexey <asboba2101@gmail.com>
2026-04-11 17:24:51 +0300

    add: schedule update entity in domain

---
+++ internal/domain/update.go
@@ -0,0 +1,40 @@
+package domain
+
+func CompareSchedules(old, current Schedule) ScheduleUpdate {
+	update := ScheduleUpdate{}
+
+	oldMap := make(map[Event]bool)
+	for _, e := range old.Events {
+		oldMap[e] = true
+	}
+
+	currMap := make(map[Event]bool)
+	for _, e := range current.Events {
+		currMap[e] = true
+	}
+
+	// Check what was deleted (was in old, but not in new)
+	for _, e := range old.Events {
+		if !currMap[e] {
+			update.Deleted = append(update.Deleted, e)
+		}
+	}
+
+	// Check what was added (is in old, but not in old)
+	for _, e := range current.Events {
+		if !oldMap[e] {
+			update.Added = append(update.Added, e)
+		}
+	}
+
+	return update
+}
+
+type ScheduleUpdate struct {
+	Added   []Event
+	Deleted []Event
+}
+
+func (u ScheduleUpdate) IsEmpty() bool {
+	return len(u.Added) == 0 && len(u.Deleted) == 0
+}
